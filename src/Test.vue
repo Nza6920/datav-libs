3f2a9e49cecdbd15f51910dc80e9f03e4680bcc8
Nza6920 <1484663282@qq.com>
2021-02-07 15:19:59 +0800

    add html中使用vue组件

--- src/Test.vue
+++ src/Test.vue
@@ -1,14 +1,33 @@
 <template>
     <div class="test">
-        {{ message }}
+        <div>
+            message: {{ message }}
+        </div>
+        <div>
+            double: {{ doubleCount }}
+        </div>
+        <button @click="add">add count</button>
     </div>
 </template>
 <script>
+import {computed, ref} from 'vue'
+
 export default {
     name: 'TestComponent',
     setup() {
+        const message = 'hello world'
+
+        // 将他变为响应式对象
+        const count = ref(1)
+        // 计算属性
+        const doubleCount = computed(() => count.value * 2)
+        const add = () => {
+            count.value ++
+        }
         return {
-            message: 'hello world'
+            message,
+            doubleCount,
+            add
         }
     }
 }

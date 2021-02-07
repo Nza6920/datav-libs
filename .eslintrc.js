b1634d69cfd686b4d5be20b77cf59a82e803dc50
Nza6920 <1484663282@qq.com>
2021-02-07 15:55:54 +0800

    add npm link关联本地组件库

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es2020": true
     },
-    "extends": "plugin:vue/essential",
+    "extends": ["plugin:vue/essential"],
     "parserOptions": {
         "ecmaVersion": 11,
         "sourceType": "module"
--- src/Test.vue
+++ src/Test.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="test">
         <div>
+            hello
+        </div>
+        <div>
             message: {{ message }}
         </div>
         <div>

b995e4ae4f26629b644da48d92835451dabf4900
Nza6920 <1484663282@qq.com>
2021-02-07 16:48:25 +0800

    add 兼容vue2

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # datav-libs
 [datav screen](https://github.com/Nza6920/datav-screen) 组件库
+
+## Project setup
+```
+npm install
+```
+
+### Link local
+```
+npm link
+```
+
+### Run
+```
+npm run dev
+```
--- src/components/Test-a/Test.vue
+++ src/components/Test-a/Test.vue
@@ -6,31 +6,14 @@
         <div>
             message: {{ message }}
         </div>
-        <div>
-            double: {{ doubleCount }}
-        </div>
-        <button @click="add">add count</button>
     </div>
 </template>
 <script>
-import {computed, ref} from 'vue'
-
 export default {
     name: 'TestComponentA',
-    setup() {
-        const message = 'hello world'
-
-        // 将他变为响应式对象
-        const count = ref(1)
-        // 计算属性
-        const doubleCount = computed(() => count.value * 2)
-        const add = () => {
-            count.value ++
-        }
+    data() {
         return {
-            message,
-            doubleCount,
-            add
+            message: 'hello world'
         }
     }
 }

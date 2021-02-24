bce8474255c1d5e753120d11c1dd86a2624c77c1
NiuZian <Nza6920@163.com>
2021-02-24 22:35:46 +0800

    merge

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import TestA from './components/Test-a/index'
 import TestB from './components/Test-b/index'
 import TestC from './components/Test-c/index'
 
-
 export default function (Vue) {
     // 嵌套使用
     Vue.use(TestA)
